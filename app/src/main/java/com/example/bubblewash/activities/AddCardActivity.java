f1ee78d4fe3a63a76b1b036a85643a9e56bc3bd4
Sandaru-Bandara <bandaraa@student.douglascollege.ca>
2024-03-25 15:34:59 -0700

    AddCardActivity

---
+++ app/src/main/java/com/example/bubblewash/activities/AddCardActivity.java
@@ -0,0 +1,16 @@
+package com.example.bubblewash.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bubblewash.R;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+    }
+}
