89d7fd1e2f2dd188f30c4790cc10a2505d221432
300366522 <fernandow3@student.douglascollege.ca>
2024-03-25 13:54:42 -0700

    Added buttons and link to login page

--- app/src/main/java/com/example/bubblewash/LoginActivity.java
+++ app/src/main/java/com/example/bubblewash/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.bubblewash;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button btnSignIn = findViewById(R.id.buttonSignIn);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
