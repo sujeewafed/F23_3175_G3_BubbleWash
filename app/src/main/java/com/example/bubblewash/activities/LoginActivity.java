12a36238dea625e5f0b2d4e5330eaa829aeba1c3
300366522 <fernandow3@douglascollege.ca>
2024-03-25 15:45:18 -0700

    Login activity developments

--- app/src/main/java/com/example/bubblewash/activities/LoginActivity.java
+++ app/src/main/java/com/example/bubblewash/activities/LoginActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.bubblewash.R;
 
@@ -16,10 +18,29 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Button btnSignIn = findViewById(R.id.buttonSignIn);
+        Button btnRegister = findViewById(R.id.buttonRegister);
+
+        EditText txtUserName = findViewById(R.id.editTextUserName);
+        EditText txtPassword = findViewById(R.id.editTextPassword);
+
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, BookingActivity.class));
+                if (txtUserName.getText().equals("") && txtPassword.getText().equals("")) {
+                    startActivity(new Intent(LoginActivity.this, BookingActivity.class));
+                }
+                else if (txtUserName.getText().equals("")){
+                    Toast.makeText(LoginActivity.this, "Please enter your user name", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Please enter your password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
     }
