fc11a4199413f5d1e51f08e53f1efa2c19c0d87e
SUJEEWA <fernandow3@student.douglascollege.ca>
2024-03-29 09:01:22 -0700

    Added User model and Dao properties and methods

--- app/src/main/java/com/example/bubblewash/interfaces/UserDao.java
+++ app/src/main/java/com/example/bubblewash/interfaces/UserDao.java
@@ -1,4 +1,19 @@
 package com.example.bubblewash.interfaces;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+
+import com.example.bubblewash.model.User;
+
+import java.util.List;
+
+@Dao
 public interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    Long[] insertUsersFromList(List<User> usersList);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertOneUser(User user);
 }
--- app/src/main/java/com/example/bubblewash/model/User.java
+++ app/src/main/java/com/example/bubblewash/model/User.java
@@ -1,4 +1,19 @@
 package com.example.bubblewash.model;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "users")
 public class User {
+    @NonNull
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    private String id;
+    @ColumnInfo(name = "username")
+    private String userName;
+
+    @ColumnInfo(name = "passowrd")
+    private String passowrd;
 }
