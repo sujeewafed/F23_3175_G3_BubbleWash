87d2937100cd255468aa8739fe60c82ef413313d
300366522 <fernandow3@student.douglascollege.ca>
2024-04-03 11:59:33 -0700

    Implemented Calendar dialog to booking page

--- app/src/main/java/com/example/bubblewash/activities/MainActivity.java
+++ app/src/main/java/com/example/bubblewash/activities/MainActivity.java
@@ -2,19 +2,47 @@ package com.example.bubblewash.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import com.example.bubblewash.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnShowCalendar;
+    private TextView txtSelectedDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         handleNavigation();
+
+        btnShowCalendar = findViewById(R.id.buttonShowCalendar);
+        txtSelectedDate = findViewById(R.id.textViewSelectedDate);
+        btnShowCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCalendarDialog();
+            }
+        });
+    }
+
+    private void openCalendarDialog(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                txtSelectedDate.setText(String.valueOf(year) + "/" + String.valueOf(month) + "/" + String.valueOf(dayOfMonth));
+            }
+        }, 2024,4,0);
+
+        datePickerDialog.show();
     }
 
     private void handleNavigation(){
